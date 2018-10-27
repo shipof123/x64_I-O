2f494c0bcbca79ce52f961b8180ce9b10dbfd513
shipof123 <34876781+shipof123@users.noreply.github.com>
2018-10-27 12:44:25 -0400

    Update puts.asm

--- puts.asm
+++ puts.asm
@@ -1,16 +1,16 @@
 section .text
 strlen:
   xor rdx, rdx
-  .loop:
-  cmp [rsi + rdx], 0
-  je .exit
+  dec rdx          ; This compensates for the INC that is happening first.
+.next:
   inc rdx
-  jmp .loop
-  .exit:
-  ret ; value in rdx
+  cmp byte [rsi + rdx], 0
+  jne .next
+  ret
 
 puts:
   ; string passed through rsi
+  call strlen      ; Result is in RDX
+
   mov rax, 1
-  call strlen
   syscall
